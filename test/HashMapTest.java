56707e9a78eafa3aabede0a12f0366e34e310944
dennis <906086497@qq.com>
2020-03-29 19:14:44 +0800

    :feat:build the project and init

---
+++ test/HashMapTest.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+/**
+ * 描述： hashmap学习
+ *
+ * @author Dennis
+ * @version 1.0
+ * @date 2020/3/28 22:47
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("k1", 1);
+        map.remove("k1");
+        System.out.println(map);
+    }
+}
